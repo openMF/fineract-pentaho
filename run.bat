3a1e6daa214e7bc213c4864f2d9f6ad075f087da
jagadeeshakn <jaga.gowdz.99@gmail.com>
2021-05-12 00:01:19 +0530

    runscript for windows

---
+++ run.bat
@@ -0,0 +1,18 @@
+@echo on
+
+rem Copy Apache Fineract JAR
+md build\run
+copy ..\fineract\fineract-provider\build\libs\fineract-provider.jar build\run
+
+rem Add our fineract-pentaho reporting plugin, like so:
+
+powershell -command "Expand-Archive -Force 'build\distributions\fineract-pentaho.zip' 'build\run'"
+del build\run\fineract-pentaho.jar
+
+md %USERPROFILE%\.mifosx\pentahoReports\
+copy build\run\pentahoReports\* %USERPROFILE%\.mifosx\pentahoReports\
+rmdir /Q /S build\run\pentahoReports
+
+rem Start!
+
+java -Dloader.path=build\run\lib\ -jar build\run\fineract-provider.jar
